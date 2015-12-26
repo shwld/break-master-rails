c4aed3cc6a7d843be961938b50d71c5d7ec8d113
shwld <shwld@outlook.com>
2015-12-26 16:33:07 +0900

    アソシエーションの設定

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,2 +1,4 @@
 class Score < ApplicationRecord
+  has_and_belongs_to_many :users
+  belongs_to :game
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_and_belongs_to_many :scores
+  belongs_to :platform
 end
